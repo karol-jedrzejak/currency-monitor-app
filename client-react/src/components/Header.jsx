988648197c8ffe6fef8ad8a1d39b688a4a44fb53
Karol Jędrzejak <karol.jedrzejak@gmail.com>
2025-10-10 10:23:12 +0200

    update

--- client-react/src/components/Header.jsx
+++ client-react/src/components/Header.jsx
@@ -21,13 +21,16 @@ const Header = () => {
 
         {/* Desktop menu */}
         <div className="hidden md:flex items-center space-x-6">
-          <a href="#home" className="rounded-md px-3 py-2 text-sm font-medium text-gray-700 hover:text-gray-900 dark:text-gray-300 dark:hover:text-gray-50">Strona główna</a>
+          <a href="#home" className="rounded-md px-3 py-2 text-sm font-medium text-gray-700 hover:text-gray-900 dark:text-gray-300 dark:hover:text-gray-50 bg-gray-100 dark:bg-gray-900 hover:bg-gray-300 dark:hover:bg-gray-700">Strona główna</a>
           <a href="#kursy" className="rounded-md px-3 py-2 text-sm font-medium text-gray-700 hover:text-gray-900 dark:text-gray-300 dark:hover:text-gray-50">Kursy walut</a>
           <a href="#kontakt" className="rounded-md px-3 py-2 text-sm font-medium text-gray-700 hover:text-gray-900 dark:text-gray-300 dark:hover:text-gray-50">Kontakt</a>
-
-          <button onClick={() => themeData.changeTheme()} className="p-2 rounded-full bg-gray-100 text-black dark:bg-gray-900 dark:text-gray-100">
-            {themeData.darkTheme ? <Sun size={18}/> : <Moon size={18}/>}
-          </button>
+        
+          {/* Theme switch */}
+          <div className="inline-flex items-stretch p-2 rounded-md bg-zinc-200 dark:bg-zinc-800 h-fit">
+            <button onClick={() => themeData.changeTheme()} className="p-1 cursor-pointer rounded-md bg-zinc-200 text-zinc-800 hover:bg-zinc-100 hover:text-zinc-900 dark:bg-zinc-800 dark:text-zinc-200 dark:hover:bg-zinc-900 dark:hover:text-zinc-100">
+              {themeData.darkTheme ? <Sun size={18}/> : <Moon size={18}/>}
+            </button>
+          </div>
         </div>
 
         {/* Mobile hamburger */}
