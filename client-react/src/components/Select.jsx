96c66f3369ef5a6f34c01081eb60976930896690
Karol Jędrzejak <karol.jedrzejak@gmail.com>
2025-11-26 14:57:03 +0100

    up

--- client-react/src/components/Select.jsx
+++ client-react/src/components/Select.jsx
@@ -14,7 +14,7 @@ const Select = ({
     <>
         <div>
             <div className="flex justify-between items-center">
-                <label className="m-2 font-medium text-gray-900 dark:text-gray-100" htmlFor={name}>{label}</label>
+                <label className="m-2 font-medium text-gray-900 dark:text-gray-100 " htmlFor={name}>{label}</label>
                 <select
                     {...(id && (id=id))}
                     className={`m-2 px-3 py-2 border dark:bg-gray-800 text-gray-900 dark:text-gray-100 border-gray-400 dark:border-gray-200 rounded-md focus:outline-none shadow-md focus:ring-3 focus:ring-emerald-500 ${className}`}
